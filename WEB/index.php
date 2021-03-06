2bc95a7baa131de403817f6a60b277885d0835fd
HoangNB1 <76941083+HoangNB1@users.noreply.github.com>
2021-03-06 19:10:29 +0700

    Update index.php

--- WEB/index.php
+++ WEB/index.php
@@ -147,9 +147,9 @@
 
                 
                 <?php
-		$hostname = "localhost";
+		$hostname = "3.93.188.184";
 		$user = "root";
-		$pass = "";
+		$pass = "123@123a";
 		$db = "tunesource";
 
 		$con = mysqli_connect($hostname,$user,$pass,$db);
